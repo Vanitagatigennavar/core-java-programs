f8ada9266a1904bb74f16edb63d0f632b48ff3ee
Vanitagatigennavar <xworkzvanita@gmail.com>
2022-06-09 18:26:25 +0530

    files added

---
+++ core-java/src/com/xworkz/constructor/constructor_chaining/car/ConstructorWithSameClass.java
@@ -0,0 +1,11 @@
+package com.xworkz.constructor.constructor_chaining.car;
+
+public class ConstructorWithSameClass {
+	public static void main(String[] args) {
+	
+	Car car=new Car();
+	System.out.println("Car model is :"+car.model);
+	System.out.println("Car color is :"+car.color);
+	System.out.println("Car price is :"+car.price);
+	}
+}
