f8223c67e7f36a0be4d8dd9df0857c2837b9c37e
Vanitagatigennavar <xworkzvanita@gmail.com>
2022-06-17 10:01:02 +0530

    files added

---
+++ core-java/src/com/xworkz/class_aptitude/ReversePyramid.java
@@ -0,0 +1,22 @@
+package com.xworkz.class_aptitude;
+
+public class ReversePyramid {
+	public static void main(String[] args)
+	{
+	int rows=5;  
+	for (int i= 0; i<= rows-1; i++)  
+	{  
+	for (int j=0; j<=i; j++)  
+	{  
+	System.out.print(" ");  
+	}  
+	for (int k=0; k<=rows-1-i; k++)  
+	{  
+	System.out.print("*" + " ");  
+	}  
+	System.out.println();  
+	}  
+	}  
+	}  
+
+
