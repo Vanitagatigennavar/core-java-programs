1328467316725e8c4c98adfd507bc3f60ac095d8
Vanitagatigennavar <xworkzvanita@gmail.com>
2022-07-19 23:55:26 +0530

    files added

--- core-java/src/com/xworkz_collectionTest/Collectiondemo2.java
+++ core-java/src/com/xworkz_collectionTest/Collectiondemo2.java
@@ -10,8 +10,20 @@ public class Collectiondemo2 {
 	al.add('B');
 	al.add('C');
 	al.add('D');
-	al.add('D');
+	al.add("vanita");
 	System.out.println(al);
+	
+	ArrayList al2=new ArrayList();
+	  al2.add("asha");
+	  al2.addAll(al);
+
+	  System.out.println(al2);
+
+	  boolean check=al2.contains("asha:");
+	  System.out.println("al2 is empty:" +al2.isEmpty());
+	  System.out.println("search asha:" +check);
+	  al2.remove("asha");
+	  al2.retainAll(al);
 
 	}
 
