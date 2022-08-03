85f69710e013fdf735baf8021b314ffff529b2b5
Vanitagatigennavar <xworkzvanita@gmail.com>
2022-08-03 11:27:42 +0530

    files added

--- bottle/src/com/xworkz/job/DAO/JobDAOimpl.java
+++ bottle/src/com/xworkz/job/DAO/JobDAOimpl.java
@@ -4,20 +4,36 @@ import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import com.xworkz.job.DTO.JobDTO;
 
 public class JobDAOimpl implements JobDAO {
 
 	@Override
-	public boolean Save() {
+	public boolean Save(JobDTO jobdto) {
+		try {
 		Connection connection =DriverManager.getConnection(URL.getvalue(),username.getvalue(),secret.getvalue());
 		String insert ="insert  job.job_imfo values(?,?,?,?,?,?)";
 		PreparedStatement statement=connection.prepareStatement(insert);
 		System.out.println(insert);
 		statement.setInt(1, JobDTO.get.Job_id());
-		statement.setString(2, JobDTO.Job_Designation.getJob);
-		
+		statement.setString(2, JobDTO.Job_Designation.getJob());
+		statement.setdouble(3, JobDTO.getpackages());
+		statement.setString(4,JobDTO.getJobqualification());
+		statement.setString(5,JobDTO.getpercentage());
+		statement.setString(6,JobDTO.getfresher());
+		int rowsaffected=statement.executeupdate();
+		if(rowsaffected>0) {
+			System.out.println("data saved:"+ rowsaffected);
+		}
+		else {
+			System.out.println("data not saved");
+			
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return false
 		
 	}
 
--- bottle/src/com/xworkz/job/constants/Designation.java
+++ bottle/src/com/xworkz/job/constants/Designation.java
@@ -1,5 +1,17 @@
 package com.xworkz.job.constants;
 
 public enum Designation {
-
+    SOFTWARE ENGINEER("software engineer"),
+    ASSOCIATE_SOFTWARE ENGINEER("asso software engineer"),
+    FULL_STACK_DEVELOPER("full stack developer"),
+    TRAINEE("trainee");
+	
+	private String name;
+	private Job_Designation(String name)
+	{
+		this.name=name;
+	}
+	public String getJob() {
+		return name;
+	}
 }
